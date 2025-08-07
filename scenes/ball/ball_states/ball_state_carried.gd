5055723c50420db72595a48be3abc7d6abaea306
ThadTheophilus <botniryo@email.com>
2025-08-07 11:08:45 +0800

    Added kick effect whilst the ball is in carried state

--- scenes/ball/ball_states/ball_state_carried.gd
+++ scenes/ball/ball_states/ball_state_carried.gd
@@ -1,8 +1,29 @@
 class_name BallStateCarried
 extends BallState
 
+const BALL_TO_PLAYER_OFFSET := Vector2(10, 4)
+const DRIBBLE_FREQUENCY := 10.0
+const DRIBBLE_INTENSITY := 4.0
+
+var dribble_time := 0.0
+
 func _enter_tree():
 	assert(carrier != null)
 
 func _physics_process(delta):
-	ball.global_position = carrier.global_position
+	var vx := 0.0
+	dribble_time += delta
+	if ball.carrier.velocity != Vector2.ZERO:
+		if carrier.velocity.x > 10:
+			vx = cos(dribble_time * DRIBBLE_FREQUENCY) * DRIBBLE_INTENSITY
+		if carrier.heading.x >= 0:
+			animation_player.play("Roll")
+			animation_player.advance(0)
+		else:
+			animation_player.play_backwards("Roll")
+			animation_player.advance(0)
+	else:
+		animation_player.play("Idle")
+	
+	ball.global_position = carrier.global_position + Vector2(vx + carrier.heading.x * BALL_TO_PLAYER_OFFSET.x, BALL_TO_PLAYER_OFFSET.y)
+		
