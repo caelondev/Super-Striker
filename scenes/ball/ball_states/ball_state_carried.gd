917becd35f0b1883b18a729a3b4e256084fb9492
ThadTheophilus <botniryo@email.com>
2025-08-07 10:13:12 +0800

    Added an ability to pickup the ball

--- scenes/ball/ball_states/ball_state_carried.gd
+++ scenes/ball/ball_states/ball_state_carried.gd
@@ -1,2 +1,8 @@
 class_name BallStateCarried
 extends BallState
+
+func _enter_tree():
+	assert(carrier != null)
+
+func _physics_process(delta):
+	ball.global_position = carrier.global_position
--- scenes/ball/ball_states/ball_state_freeform.gd
+++ scenes/ball/ball_states/ball_state_freeform.gd
@@ -1,2 +1,9 @@
 class_name BallStateFreeform
 extends BallState
+
+func _enter_tree():
+	player_detection_area.body_entered.connect(on_player_pickup.bind())
+
+func on_player_pickup(body: Player) -> void:
+	ball.carrier = body
+	state_transition_requested.emit(Ball.State.CARRIED)
