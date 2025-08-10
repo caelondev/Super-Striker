392ec71d0424f54c4fb5d12e93303bdf3329a544
ThadTheophilus <botniryo@email.com>
2025-08-10 20:39:10 +0800

    Added camera movement

---
+++ scenes/camera.gd
@@ -0,0 +1,16 @@
+class_name Camera
+extends Camera2D
+
+const DISTANCE_TARGET := 100
+const SMOOTHING_BALL_CARRIED = 2
+const SMOOTHING_BALL_DEFAULT = 5
+
+@export var ball : Ball
+
+func _physics_process(_delta):
+	if ball.carrier != null:
+		position_smoothing_speed = SMOOTHING_BALL_CARRIED
+		global_position = ball.carrier.global_position + ball.carrier.heading * DISTANCE_TARGET
+	else:
+		position_smoothing_speed = SMOOTHING_BALL_DEFAULT
+		global_position = ball.global_position
