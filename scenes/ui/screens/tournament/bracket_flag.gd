17a939c94c7bf0d07f6127ab6b6c004451b474a8
caelondev <jerichopanagsagan0@gmail.com>
2025-08-24 01:41:00 +0800

    Added TournamentUI Structure

---
+++ scenes/ui/screens/tournament/bracket_flag.gd
@@ -0,0 +1,15 @@
+class_name BracketFlag
+extends TextureRect
+
+@onready var border : TextureRect = %Border
+@onready var score_label : Label = %ScoreLabel
+
+func set_as_current_team() -> void:
+	border.show()
+
+func set_as_winner(score: String) -> void:
+	score_label.text = score
+	score_label.show()
+
+func aet_as_loser() -> void:
+	modulate = Color(0.2, 0.2, 0.2, 1)
